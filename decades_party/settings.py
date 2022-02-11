cbfebce4edf8c9b8331d725422d04840da3bb8a6
Berkay Alatas <berkay.3304@gmail.com>
2022-02-12 00:33:35 +0300

    Created Party App - Music Controller

--- decades_party/settings.py
+++ decades_party/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8(kcr=31tc0t+5y#&x*3f0$&p@mv7ypv=c3slr+7&^^bh_^v$a'
+SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,5 @@
+import os
+
 CLIENT_ID = '5d38e6a5e2f74301bba3d53ae9a690b0'
-CLIENT_SECRET = '0dc92e6541264293bde5ffc7f2caf30d'
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET_SPOTIFY')
 REDIRECT_URI = 'http://127.0.0.1:8000/spotify/redirect'
